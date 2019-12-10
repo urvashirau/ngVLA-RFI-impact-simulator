426c1105d5e54f95341dd9f397819e5c7f72a36e
Urvashi Rao Venkata <rurvashi@nrao.edu>
2019-12-10 12:44:50 -0500

    Replace ngvlapar.py

--- Code/ngvlapar.py
+++ Code/ngvlapar.py
@@ -29,40 +29,47 @@ if arrtype=='basic':
 if arrtype=='ngvla' or arrtype=='approx_ngvla':
     
     ## ngVLA Definition
-    ##   100  x  18m  :  Core  ( 1km baselines )
-    ##   114 x 18m  : Plains Spiral  ( 70 km baselines )
+    ##   94  x  18m  :  Main : Core  ( 1.25 km baselines )
+    ##   74 x 18m  : Main : Plains Spiral  ( 70 km baselines )
+    ##   46 x 18m : Main : Mid Baselines (upto 1000km )
     ##   19 x 6m  : Compact Core ( 0.1 km baselines )
     ##   233 in total so far.
     ##   30 x 18m  : Long Baselines  ( 1000km baselines, in 10 clusters/stations )
     arrayfracinfo={
-        'core': {'nant':119.0, 'maxbase':1.0},   ## Core and compact core
-        'outlier':{'nant':30+20, 'maxbase':1000.0},    ## 
-#        'full': {'nant':233, 'maxbase':100.0}
-        'full': {'nant':263, 'maxbase':1000.0}
+        'core': {'nant':94+19+30, 'maxbase':5.0},   ## 143 Core and compact core and 20 plains ants
+        'outlier':{'nant':30, 'maxbase':1000.0},    ## baselines that all see the RFI.
+        'full': {'nant':263, 'maxbase':1000.0} ## Main and Compact arrays
     }
     totalants = 263
     totalvis = totalants * ( totalants - 1)/2.0
-    maxbase = 1000.0 # km
+    maxbase = 8000.0 # km
     ## Baseline count profile
     base_lengths = np.arange(0.02,maxbase,0.1)   # 1-500 km baselines
     base_counts = base_lengths.copy()
     
     ## Relative numbers of baselines for each baseline length range.
     if arrtype=='approx_ngvla':
-        base_counts.fill(0.5)
-        base_counts[ base_lengths < 1.0 ] = 1.0
-        base_counts[ base_lengths >=100  ] = 0.1
-
-    else:
         ### 119 within 1km baselines
         ### 114 in spirals out to 100k baselines
         ### 30 outliers upto 1000k baselines
         nbase_upto_1k = 119*118/2.0
         nbase_1k_to_100k = 114*113/2.0 + 114*119  
         nbase_beyond_100k = 30*29/2.0 + 30*233
+
         base_counts.fill( nbase_1k_to_100k )
         base_counts[ base_lengths < 1.0 ] = nbase_upto_1k
         base_counts[ base_lengths >=100  ] = nbase_beyond_100k
+
+    else:   ## main ngvla ( arrtype = 'ngvla' )
+
+        nbase_upto_5k = 143*142/2.0   # core : 143 ants
+        nbase_5k_to_1000k = 90*89.0/2.0 + 90*143  # 233 - 143 = 90 (rest of plains, and midbase)
+        nbase_beyond_1000k = 30*29/2.0 + 30*233  # all long baselines
+
+        base_counts.fill( nbase_5k_to_1000k )
+        base_counts[ base_lengths < 5.0 ] = nbase_upto_5k
+        base_counts[ base_lengths >=1000  ] = nbase_beyond_1000k
+
     
 ## Normalize such that the integrated total is 'totalvis'
 base_counts = (base_counts / np.sum(base_counts)) * totalvis
